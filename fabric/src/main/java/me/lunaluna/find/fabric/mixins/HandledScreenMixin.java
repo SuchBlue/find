1518812b421b7aa71c553b1eb882670096c3f2ff
SuchBlue <58862188+SuchBlue@users.noreply.github.com>
2025-07-17 13:07:46 +0200

    Updated for 1.21.5

--- fabric/src/main/java/me/lunaluna/find/fabric/mixins/HandledScreenMixin.java
+++ fabric/src/main/java/me/lunaluna/find/fabric/mixins/HandledScreenMixin.java
@@ -7,12 +7,14 @@ import me.lunaluna.find.fabric.widget.FindWidget;
 import net.minecraft.client.gui.DrawContext;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.client.gui.screen.ingame.HandledScreen;
+import net.minecraft.client.render.RenderLayer;
 import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.screen.slot.Slot;
 import net.minecraft.text.Text;
 import org.lwjgl.glfw.GLFW;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Shadow;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -63,14 +65,22 @@ public abstract class HandledScreenMixin extends Screen {
     private void renderSearch(DrawContext matrices, int mouseX, int mouseY, float delta, CallbackInfo ci) {
         widget.render(matrices, mouseX, mouseY, delta);
     }
+
+    @Unique
     private void darkenSlot(DrawContext matrices, int x, int y) {
         var color = Config.INSTANCE.color().getRGB();
         var border = HAS_BORDER ? 1 : 0;
 
-        RenderSystem.disableDepthTest();
-        RenderSystem.colorMask(true, true, true, false);
+        float r = (color >> 16 & 255) / 255.0F;
+        float g = (color >> 8 & 255) / 255.0F;
+        float b = (color & 255) / 255.0F;
+        float a = 0.2F;
+
+        matrices.getMatrices().push();
+        RenderSystem.setShaderColor(r, g, b, a);
+
         matrices.fillGradient(x - border, y - border, x + 16 + border, y + 16 + border, 100, color, color);
-        RenderSystem.colorMask(true, true, true, true);
-        RenderSystem.enableDepthTest();
+        RenderSystem.setShaderColor(1F, 1F, 1F, 1F);
+        matrices.getMatrices().pop();
     }
 }
